ca198ad0f986cccdc3b62e5675a2e0464d6477c7
Waqas <ssuet_waqas@hotmail.com>
2017-05-03 16:41:27 +0500

    Update kaku.h

--- kaku.h
+++ kaku.h
@@ -1,5 +1,7 @@
+
 #include <NewPing.h>
 #define MAX_distanceANCE 200
+#define MAX_DISTANCE_FROM_OBSTACLE_IN_CMS 20
 const int leftMotorDirection = 4;
 const int leftMotorSpeed = 5;
 const int rightMotorSpeed = 6;
@@ -9,13 +11,10 @@ const int SonarEcho = 10;
 const int SonarTrig = 9;
 const int SonarGnd = 11;
 NewPing sonar(SonarTrig, SonarEcho, MAX_distanceANCE);
-
-#define FORWARD LOW
-#define REVERSE HIGH
+#define FORWARD HIGH
+#define REVERSE LOW
 #define STOP LOW
-
 #define sonarSensor sonar.ping_cm()
-
 void setupRobot(void)
 {
   pinMode(SonarVcc, OUTPUT);
@@ -23,95 +22,89 @@ void setupRobot(void)
   digitalWrite(SonarVcc, HIGH);
   digitalWrite(SonarGnd, LOW);
   Serial.begin(9600);
-
   pinMode(leftMotorDirection, OUTPUT);
   pinMode(leftMotorSpeed, OUTPUT);
   pinMode(rightMotorDirection, OUTPUT);
   pinMode(rightMotorSpeed, OUTPUT);
 }
-
 long duration, inches, cm;
-
 void setup()
-
 {
 setupRobot();
-Serial.begin(9600);
-
 pinMode(SonarTrig,OUTPUT);
-
 pinMode(SonarEcho,INPUT);
-
-pinMode(leftMotorDirection, OUTPUT);
-pinMode(leftMotorSpeed, OUTPUT);
-pinMode(rightMotorDirection, OUTPUT);
-pinMode(rightMotorSpeed, OUTPUT);
-
 }
-
+int count = 0;
 void loop()
-
 {
-
 digitalWrite(SonarTrig, LOW);
-
 delayMicroseconds(2);
-
 digitalWrite(SonarTrig, HIGH);
-
 delayMicroseconds(5);
-
 digitalWrite(SonarTrig, LOW);
-
 duration = pulseIn(SonarEcho, HIGH);
-
 //this returns the time duration taken
-
 //for the ultrasonics to hit an obstacle and return
-
-inches = duration / 74 / 2;//converts the time duration into inches
-
-cm = duration / 29 / 2;//converts the time duration to cm
-
-if(cm>30)//checks for the distance is greater than 4cm
+unsigned int distanceInCms = sonar.ping() / US_ROUNDTRIP_CM; // Send ping, get ping time in microseconds (uS) and convert it to centimeters (cm)
+Serial.println("Distance: " + String(distanceInCms) + "cm");
+  if(distanceInCms != NO_ECHO && distanceInCms < MAX_DISTANCE_FROM_OBSTACLE_IN_CMS) 
+  {
+    goBackward(1000);
+    count++;
+    if(count>30)
+    {
+      if(rand()%2==0)
+        turnLeft(1000);
+      else
+        turnRight(2000);
+       count = 0;
+    }
+    else
+    {
+  stoP(3000);
+  }
+  }
+  else
+  {
+     goForward(4000);
+  }
+}
+int distance = 0;
+void goForward(int d)
 {
-//the bot forward if the condition is true
-
-digitalWrite(leftMotorDirection ,HIGH);//Both the motors are in ON state
-digitalWrite(leftMotorSpeed ,HIGH);//Both the motors are in ON state
-
-digitalWrite(rightMotorDirection ,HIGH);
-digitalWrite(rightMotorSpeed ,HIGH);//Both the motors are in ON state
-
-Serial.print("No obstacle detected");
+    digitalWrite(leftMotorSpeed, HIGH);
+    digitalWrite(leftMotorDirection, FORWARD);
+    digitalWrite(rightMotorSpeed, HIGH);
+    digitalWrite(rightMotorDirection, FORWARD);  
+    delay(d);
 }
-
-else
- {
-  
-//the robot turns right when an obstacle is detected
-
-digitalWrite(leftMotorDirection ,LOW);//the left motor is in ON state
-digitalWrite(rightMotorDirection ,HIGH);//the right motor is in OFF state
-digitalWrite(leftMotorSpeed ,LOW);//Both the motors are in ON state
-digitalWrite(rightMotorSpeed ,HIGH);//Both the motors are in ON state
-
-Serial.print("Obstacle detected");
-
-delay(100);//this delays the process by 100millisec
-
-Serial.print(inches);
-
-Serial.print("in, ");
-
-Serial.print(cm);
-
-Serial.print("cm");
-
-Serial.println();
-
-delay(100);//this delays the code by 0.1 second and repeats the loop again
-
+void turnLeft(int d)
+{
+    digitalWrite(leftMotorSpeed, HIGH);
+    digitalWrite(leftMotorDirection, REVERSE);
+    digitalWrite(rightMotorSpeed, HIGH);
+    digitalWrite(rightMotorDirection, FORWARD);  
+    delay(d);
 }
-delay(500);
+void goBackward(int d)
+{
+    digitalWrite(leftMotorSpeed, HIGH);
+    digitalWrite(leftMotorDirection, REVERSE);
+    digitalWrite(rightMotorSpeed, HIGH);
+    digitalWrite(rightMotorDirection, REVERSE);  
+    delay(d);
+}
+void turnRight(int d)
+{
+    digitalWrite(leftMotorSpeed, HIGH);
+    digitalWrite(leftMotorDirection, FORWARD);
+    digitalWrite(rightMotorSpeed, HIGH);
+    digitalWrite(rightMotorDirection, REVERSE);  
+    delay(d);
+}
+void stoP(int d)
+{
+    digitalWrite(leftMotorSpeed, LOW);
+    digitalWrite(rightMotorSpeed, LOW);
+    delay(d);
 }
